dee1f0780cf50106d29fda7e9c3a85d7b8dec454
tringuyen <nguyenhuunhantri2003@gmail.com>
2024-07-09 14:38:00 +0700

    BE

--- BE/config/db.config.js
+++ BE/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "localhost",
-    PORT: 27017,
-    DB: "DB_Ticket"
+    HOST: process.env.DB_HOST || "localhost",
+    PORT: process.env.DB_PORT || 27017,
+    DB: process.env.DB_NAME || "DB_Ticket"
 };
